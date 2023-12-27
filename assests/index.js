2f9941ee3d867891c2009db9717544f497bb5694
Md. Faisal Mahmud <123616694+Faisal7822@users.noreply.github.com>
2023-12-27 23:55:30 +0600

    sailor sailing clone

---
+++ assests/index.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+//     $('#icon').click(function(){
+//      $('sub-menu').toggleClass('show');
+//     });
+// });
+
+let navtoggle = document.querySelector(".nav-toggle");
+let sidebar = document.querySelector(".sidebar");
+
+navtoggle.addEventListener("click" , function(){
+    if(navtoggle.firstElementChild.classList.contains("fa-bars"))
+    {
+        navtoggle.firstElementChild.classList.replace("fa-bars","fa-times");
+    }
+    else{
+        navtoggle.firstElementChild.classList.replace("fa-times","fa-bars");
+    }
+
+    sidebar.classList.toggle("show_sidebar");
+    // navtoggle.firstElementChild.classList.replace("fa-bars","fa-times");
+})
